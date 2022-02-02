13e61541df0a40bd8947bc1cdd8b0d544e5a2c6f
armpaster <98588904+armpaster@users.noreply.github.com>
2022-02-02 21:50:59 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+   function openPage() {
+            var x = document.getElementById("search").value;
+
+            if (x === "մանե") {
+                window.open("images/d.png")                                                                                                                                                                                 
+            }
+            if (x === "p text") {
+            	window.open("images/logo.png")
+            }
+
+            if (x === "facebook") {
+                window.open("https://www.facebook.com/");
+            }
+if (x === "մարդ") {
+                window.open("https://www.facebook.com/");
+            }
+if (x === "մարդ") {
+                window.open("https://www.facebook.com/");
+            }
+if (x === "մարդ") {
+                window.open("#");
+            }
+if (x === "մարդ") {
+                window.open("#");
+            }
+if (x === "մարդ") {
+                window.open("#");
+            }
+if (x === "մարդ") {
+                window.open("#");
+            }
+if (x === "մարդ") {
+                window.open("#");
+            }
+if (x === "մարդ") {
+                window.open("#");
+            }
+if (x === "մարդ") {
+                window.open("#");
+            }
+if (x === "մարդ") {
+                window.open("#");
+            }
+if (x === "մարդ") {
+                window.open("#");
+            }
+if (x === "մարդ") {
+                window.open("#");
+            }
+if (x === "մարդ") {
+                window.open("#");
+            }
+if (x === "մարդ") {
+                window.open("#");
+            }
+if (x === "մարդ") {
+                window.open("#");
+            }
+if (x === "մարդ") {
+                window.open("#");
+            }
+
+        }
